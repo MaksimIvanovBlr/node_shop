524a87b2c1b7685c5f34d98a46f2cfdd8e0d29af
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-03-08 17:04:01 +0300

    add routs

--- server/index.js
+++ server/index.js
@@ -4,18 +4,20 @@ const sequelize = require('./db')
 const models = require('./models/models')
 
 const cors = require('cors')
+const router = require('./routes/index')
 const PORT = process.env.PORT || 5000
 const app = express()
 
 
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
 
-app.get('/', (req, res) => {
-    res.status(200).json({messaage:'WORKING!!!'})
+// app.get('/', (req, res) => {
+//     res.status(200).json({messaage:'WORKING!!!'})
     
-})
+// })
 
 
 
