d6ef0f074bff57ca84c9605d30b3b2abfaa07246
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-03-08 18:37:23 +0300

    add midellwares and controllers

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ const cors = require('cors')
 const router = require('./routes/index')
 const PORT = process.env.PORT || 5000
 const app = express()
-
+const errorHeandler = require('./middleware/ErrorHeandlerMiddleware')
 
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
-
+app.use(errorHeandler)
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({messaage:'WORKING!!!'})
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,13 +1,12 @@
 const Router = require('express')
+const userController = require('../controllers/userController')
 const router = new Router()
+const UserController = require('../controllers/userController')
 
 
-
-router.post('/registration',)
-router.post('/login ',)
-router.get('/auth', (req, res) => {
-    res.json({message:'All working'})
-})
+router.post('/registration', userController.registration)
+router.post('/login ', userController.login)
+router.get('/auth', userController.check)
 
 
 module.exports = router
