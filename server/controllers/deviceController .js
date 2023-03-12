6a6dea891ccfc12deeb7bba29ce64a24110d9d6f
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-03-12 13:31:15 +0300

    done crudl

--- server/controllers/deviceController .js
+++ server/controllers/deviceController .js
@@ -1,7 +1,7 @@
 const { UUIDV4 } = require('sequelize')
 const uuid = require('uuid')
 const path = require('path')
-const {Device} = require('../models/models')
+const {Device, DeviceInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 
@@ -11,11 +11,28 @@ class DeviceController {
     async create(req, res, next) {
 
         try {
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
+            
             const device = await Device.create({name, price, brandId, typeId, img:fileName})
+
+
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach(i => 
+                  DeviceInfo.create({
+                    title: i.title,
+                    description: i.description,
+                    deviceId: device.id 
+
+                  })  
+                )
+            }
+
+
+
 
             return res.json(device)
         } catch (error) {
@@ -51,7 +68,14 @@ class DeviceController {
     }
 
     async getOne(req, res) {
-        
+        const {id} = req.params
+        const device = await Device.findOne(
+            {
+                where:{id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            }
+        )
+        return res.json(device)
     }
 }
 
