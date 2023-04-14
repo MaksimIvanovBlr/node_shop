2330686041e461e7044f07a73a9cb36e3c4e3df4
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-04-14 18:32:46 +0300

    auth redir. to login route/ logout

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -13,19 +13,26 @@ import { useNavigate } from 'react-router-dom';
 const NavBar = observer(() => {
     const {user} = useContext(Context)
     const navigate = useNavigate()
+
+    const logOut = () => {
+      user.setUser({})
+      user.setIsAuth(false)
+    }
+
+
     return (
 <Navbar bg="dark" variant="dark">
         <Container>
           <NavLink to={SHOP_ROUTE}> МагазИн</NavLink>
           {user.isAuth ?
           <Nav className="le-auto">
-            <Button variant={'outline-light'} onClick={() => navigate(LOGIN_ROUTE)}>Log out</Button>
+            <Button variant={'outline-light'} onClick={() => logOut()}>Log out</Button>
             <Button variant={'outline-light'} onClick={() => navigate(ADMINE_ROUTE)}>Admin panell</Button>
 
           </Nav>
           :
           <Nav className="le-auto">
-            <Button  className="ml-2" variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Autorization</Button>
+            <Button  className="ml-2" variant={'outline-light'} onClick={() => navigate(LOGIN_ROUTE)}>Autorization</Button>
 
           </Nav>
           }
