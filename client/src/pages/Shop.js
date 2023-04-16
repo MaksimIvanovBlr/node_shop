7dbb405d63dce648532ec6756150569374924fd4
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-04-16 14:02:51 +0300

    pagination

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -6,6 +6,7 @@ import DeviceList from '../components/DeviceList';
 import { observer } from 'mobx-react-lite';
 import { Context } from '..';
 import { fetchBrands, fetchDevices, fetchTypes } from '../http/deviceAPI';
+import Pages from '../components/Pages';
 
 const Shop = observer(() => {
   const {device} = useContext(Context)
@@ -13,8 +14,19 @@ const Shop = observer(() => {
   useEffect( () => {
     fetchTypes().then( data => device.setTypes(data))
     fetchBrands().then( data => device.setBrands(data))
-    fetchDevices().then( data => device.setDevices(data.rows))
+    fetchDevices(null, null, 1, 3).then( data => {
+      device.setDevices(data.rows)
+      device.setTotalCount(data.count)
+    })
   }, [])
+
+  useEffect( () => {
+    fetchDevices(device.selectedType.id, device.selectedBrand.id, device.page, 3).then( data => {
+      device.setDevices(data.rows)
+      device.setTotalCount(data.count)
+    })
+
+  }, [device.page, device.selectedType, device.selectedBrand,])
   return (
     <Container>
       <Row className='mt-2'>
@@ -25,6 +37,7 @@ const Shop = observer(() => {
         <Col md={9}>
           <BrandBar/>
           <DeviceList/>
+          <Pages/>
         </Col>
 
       </Row>
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -23,6 +23,11 @@ export default class DeviceStore{
         ]
         this._selectedType = {}
         this._selectedBrand = {}
+        this._page = 1
+        this._totalCount = 0
+        this._limit = 3
+
+
         makeAutoObservable(this)
     }
 
@@ -37,12 +42,24 @@ export default class DeviceStore{
      }
 
      setSelectedType(type){
+      this.setPage(1)
       this._selectedType = type
      }
 
      setSelectedBrand(brand){
+      this.setPage(1)
       this._selectedBrand = brand
      }
+
+     setPage(page){
+      this._page = page
+     }
+
+     setTotalCount(count){
+      this._totalCount = count
+     }
+
+     
 
 
 
@@ -64,4 +81,15 @@ export default class DeviceStore{
      get selectedBrand(){
       return this._selectedBrand 
      }
+
+     get totalCount() {
+      return this._totalCount
+     } 
+     
+     get page() {
+      return this._page
+      }
+     get limit() {
+      return this._limit
+      }
 }
